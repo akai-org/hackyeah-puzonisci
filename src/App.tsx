56ead167785d9360fc57d66db2045dcd51d3c9d2
Now4czyk <kacpern30@gmail.com>
2022-11-19 13:37:48 +0100

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Button } from '@chakra-ui/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Button>Hello HackYeah</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/info" element={<div>info</div>} />
+        <Route path="/counter" element={<div>counter</div>} />
+        <Route path="/game" element={<div>giera</div>} />
+        <Route path="/*" element={<div>not found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
