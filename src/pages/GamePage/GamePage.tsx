99fd6c6a50aaf17f7ea5feee63569d668466075b
Patryk Marczak <m.patryk@outlook.com>
2022-11-19 13:52:20 +0100

    Delete useless react imports

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import { Card, CardBody, Text } from '@chakra-ui/react';
 
-export const GamePage = () => {
-  return <div>game page</div>;
+export const Game = () => {
+  return (
+    <Card>
+      <CardBody>
+        <Text>View a summary of all your customers over the last month.</Text>
+      </CardBody>
+    </Card>
+  );
 };
