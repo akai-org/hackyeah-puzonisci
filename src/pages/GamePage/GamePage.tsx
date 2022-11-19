e670d76b9c3a2004d9c386bb50c4fc8317e738ef
Patryk Marczak <m.patryk@outlook.com>
2022-11-19 15:06:01 +0100

    Game header

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,12 +1,35 @@
-import { Card, CardBody, Text } from '@chakra-ui/react';
+import { Button, Card, CardBody, Text } from '@chakra-ui/react';
 import styles from './GamePage.module.scss';
+import { useState } from 'react';
+
+const moneyPerVertilizer = 1;
 
 export const GamePage = () => {
+  const [vertilizer, setVertilizer] = useState(1000);
+  const [money, setMoney] = useState(0);
+
+  const transferVertilizer = () => {
+    setMoney((money) => money + vertilizer * moneyPerVertilizer);
+    setVertilizer(0);
+  };
+
   return (
     <main className={styles.gameWrapper}>
       <Card className={styles.mainGameCard}>
-        <CardBody>
-          <Text>View a summary of all your customers over the last month.</Text>
+        <CardBody className={styles.cardBody}>
+          <section className={styles.headerModule}>
+            <Text noOfLines={1} className={styles.moneyDisplay}>
+              {vertilizer}kg
+            </Text>
+            <Button
+              onClick={transferVertilizer}
+              className={styles.transferVertilizer}
+              colorScheme="green"
+            >
+              Transfer to money
+            </Button>
+            <Text className={styles.moneyDisplay}>{money}zł</Text>
+          </section>
         </CardBody>
       </Card>
     </main>
